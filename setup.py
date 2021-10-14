958a63cce8f28ba37d9b65fdeace4c434868df09
Wansoo Ha <wansooha@gmail.com>
2021-10-14 14:01:01 +0900

    setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+import os
+
+setuptools.setup(
+        name='velgen',
+        version='0.1',
+        description='Random seismic velocity generator',
+        author='Wansoo Ha',
+        author_email='wansooha@gmail.com',
+        packages=setuptools.find_packages()
+)
